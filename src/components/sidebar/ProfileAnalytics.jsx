58c496742e4ec1e15954bce7db88c6d054d58ea9
Abhishek01454 <abhisheksharmaa1404@gmail.com>
2026-01-09 11:31:12 +0530

    Add ESLint ignore comments for unused Framer Motion imports

--- src/components/sidebar/ProfileAnalytics.jsx
+++ src/components/sidebar/ProfileAnalytics.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+// eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
 import { useUI } from "../../context/AppContext";
 import { Eye, BarChart3 } from "../icons";
